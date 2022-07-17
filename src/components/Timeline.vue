c154c33845f7c5b327efee44211850ea5161dcd5
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-07-17 17:50:44 +0300

    add timline post component, add new interface, render filtered posts

--- src/components/Timeline.vue
+++ src/components/Timeline.vue
@@ -1,22 +1,37 @@
 <script setup lang="ts">
 import { DateTime } from "luxon";
-import { ref } from "vue";
-import { Post, today, thisWeek, thisMonth } from "../posts";
+import { ref, computed } from "vue";
+import { TimelinePost, today, thisWeek, thisMonth } from "../posts";
+import TimelineItem from "./TimelineItem.vue";
 
 const periods = ["Today", "This Week", "This Month"] as const;
 type Period = typeof periods[number];
 
-const currentPeriod = ref<Period>("Today");
+const selectedPeriod = ref<Period>("Today");
 
 function selectPeriod(period: Period) {
-  currentPeriod.value = period;
+  selectedPeriod.value = period;
 }
 
-const posts = [today, thisWeek, thisMonth].map((post) => {
-  return {
-    ...post,
-    created: DateTime.fromISO(post.created),
-  };
+const posts = computed<TimelinePost[]>(() => {
+  return [today, thisWeek, thisMonth]
+    .map((post) => {
+      return {
+        ...post,
+        created: DateTime.fromISO(post.created),
+      };
+    })
+    .filter((post) => {
+      if (selectedPeriod.value === "Today") {
+        return post.created >= DateTime.now().minus({ day: 1 });
+      }
+
+      if (selectedPeriod.value === "This Week") {
+        return post.created >= DateTime.now().minus({ week: 1 });
+      }
+
+      return post;
+    });
 });
 </script>
 
@@ -26,15 +41,12 @@ const posts = [today, thisWeek, thisMonth].map((post) => {
       <a
         v-for="period in periods"
         :key="period"
-        :class="{ 'is-active': period === currentPeriod }"
+        :class="{ 'is-active': period === selectedPeriod }"
         @click="selectPeriod(period)"
         >{{ period }}</a
       >
     </span>
 
-    <a v-for="post of posts" :key="post.id">
-      <a>{{ post.title }}</a>
-      <div>{{ post.created.toFormat("d MMM") }}</div>
-    </a>
+    <TimelineItem v-for="post of posts" :key="post.id" :post="post" />
   </nav>
 </template>
