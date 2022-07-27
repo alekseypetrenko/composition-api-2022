aaef960b11ea7ff8fac18cf38a46ea19a166199a
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-07-27 21:50:56 +0300

    add newpost router and navbar

--- src/main.ts
+++ src/main.ts
@@ -4,4 +4,4 @@ import { router } from "./router";
 import "./style.css";
 import App from "./App.vue";
 
-createApp(App).use(createPinia()).use(router).mount("#app");
+createApp(App).use(router).use(createPinia()).mount("#app");
