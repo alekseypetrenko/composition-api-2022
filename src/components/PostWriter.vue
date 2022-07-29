91f859d29c22554e828d4a395f5092ce78495f01
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-07-29 21:44:10 +0300

    Added contenEditable div

---
+++ src/components/PostWriter.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="columns">
+    <div class="column">
+      <div class="field">
+        <div class="label">{{ title }}</div>
+        <input type="text" class="input" v-model="title" />
+      </div>
+      <div contenteditable ref="contentEditable">Tet</div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts" setup>
+import { ref, onMounted } from "vue";
+import { TimelinePost } from "../posts";
+
+const props = defineProps<{
+  post: TimelinePost;
+}>();
+
+const title = ref(props.post.title);
+const contentEditable = ref<HTMLDivElement>();
+
+onMounted(() => {
+  console.log(contentEditable.value?.innerText);
+});
+</script>
--- src/views/NewPost.vue
+++ src/views/NewPost.vue
@@ -1,1 +1,15 @@
-<template>New</template>
+<script lang="ts" setup>
+import PostWriter from "../components/PostWriter.vue";
+import { TimelinePost } from "../posts";
+import { DateTime } from "luxon";
+
+const post: TimelinePost = {
+  id: "-1",
+  title: "Dummy title",
+  created: DateTime.now(),
+};
+</script>
+
+<template>
+  <post-writer :post="post" />
+</template>
