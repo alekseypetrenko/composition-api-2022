96861585f442ce7b0f9d113b8476f344c9ccaa56
Oleksii Petrenko <oleksii.petrenko@valtech.com>
2022-08-10 23:12:15 +0300

    auth and fetch post before router

--- src/main.ts
+++ src/main.ts
@@ -3,5 +3,17 @@ import { createPinia } from "pinia";
 import { router } from "./router";
 import "./style.css";
 import App from "./App.vue";
+import { useUsers } from "./stores/users";
+import { usePosts } from "./stores/posts";
 
-createApp(App).use(router).use(createPinia()).mount("#app");
+const app = createApp(App);
+
+app.use(createPinia());
+
+const usersStore = useUsers();
+const postStore = usePosts();
+
+Promise.all([usersStore.authenticate(), postStore.fetchPosts()]).then(() => {
+  app.use(router);
+  app.mount("#app");
+});
